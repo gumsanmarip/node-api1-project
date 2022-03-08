3936ccd3bfecf494e4d4180a1850b825665f01e7
gumsanmarip <gumsanmarip@gmail.com>
2022-03-07 20:47:38 -0500

    start

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+server.use(express.json());
+
+server.get('/json', (req, res) => {
+    res.json({ a: 1, b: 2, c: 3})
+});
